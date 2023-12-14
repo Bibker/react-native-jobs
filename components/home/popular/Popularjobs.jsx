305a8050aa4ebe49a02ee3aec08217e6865528ed
Bibek Bhusal <iambibker@gmail.com>
2023-12-14 17:49:55 +0545

    Job Search Implemented

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -16,6 +16,8 @@ const Popularjobs = () => {
 
   const [selectedJob, setSelectedJob] = useState();
   const handleCardPress = (item) => {
+    router.push(`job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
 
   }
 
@@ -37,6 +39,7 @@ const Popularjobs = () => {
             data={data}
             renderItem={({item})=> (
               <PopularJobCard
+                handleCardPress={handleCardPress}
                 item={item}
               />
             )}
